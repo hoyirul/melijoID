226fd33ea20c051ca88c474800fcd3d9e342dcfe
Mochammad Hairullah <irulgraspama621@gmail.com>
2022-11-10 21:07:40 +0700

    Add Column on Carts

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -9,7 +9,7 @@ class Cart extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['user_customer_id', 'product_id', 'quantity'];
+    protected $fillable = ['user_customer_id', 'product_id', 'quantity', 'grouping'];
 
     public function user_customer(){
         return $this->belongsTo(UserCustomer::class, 'user_customer_id', 'id');
